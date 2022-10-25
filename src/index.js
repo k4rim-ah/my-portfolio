5f879e2b54841646b6e15831e58013a43248642f
k4rim-ah <karimahmediphone@gmail.com>
2022-10-25 09:40:39 +0200

    tuesday update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
